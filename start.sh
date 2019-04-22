6a1be52bec719006c346a0591e9729be1123d1b4
Guy Salton <47520615+guysalton-codefresh@users.noreply.github.com>
2019-04-22 18:10:56 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,9 +8,8 @@ rm -rf /codefresh/volume/${CF_REPO_NAME} || true
 git config --global credential.helper 'store --file ~/.my-credentials'
 echo https://${GIT_USER}:${GIT_PASSWORD}@git-codecommit.${REGION}.amazonaws.com >> ~/.my-credentials
 
-# cloning the repo and 
+# cloning the repo
 git clone https://git-codecommit.${REGION}.amazonaws.com/v1/repos/${CF_REPO_NAME} /codefresh/volume/${CF_REPO_NAME}
-cd ${CF_REPO_NAME}
 
 # checking out branch
 git checkout ${CF_BRANCH}
